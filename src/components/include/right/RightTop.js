3f6e11979ae51f09eeed859650319833ab23a9ba
chanu <cw8881@naver.com>
2023-08-13 05:17:43 +0900

    우측 이웃목록 컴포넌트화

---
+++ src/components/include/right/RightTop.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from "react-router-dom";
+import NeighborList from './NeighborList';
+import ButtonRight from './ButtonRight';
+
+const RightTop = () => {
+    return (
+        // <!-- 우측 -->
+
+        <section id="neighbor" className="neighbor w-full h-[50%] max-h-[500px] rounded-[20px] overflow-hidden shadow-lg bg-[#FAFAFA] ">
+            <div className="px-6 py-5">
+                <div className="font-bold text-xl mb-2">이웃 목록</div>
+
+                <NeighborList />
+                <NeighborList />
+                <NeighborList />
+
+                <ButtonRight />
+
+
+            </div>
+
+        </section>
+
+    )
+}
+
+export default RightTop;
