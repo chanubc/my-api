c0310a2d18e53bc824d707cbbb9ea2a068b562ac
chanu <cw8881@naver.com>
2023-08-13 05:18:47 +0900

    이웃목록 hover

--- src/components/include/right/NeighborList.js
+++ src/components/include/right/NeighborList.js
@@ -1,14 +1,19 @@
 import React, { useEffect, useState } from 'react';
+import '../../css/home.css';
+
 
 const NeighborList = () => {
     return (
         <>
-            <section class="flex items-center">
-                <img class="w-10 h-10 rounded-full mr-4 bg-black" src="/img/jonathan.jpg" alt="Avatar of Jonathan Reinink" />
-                <div class="text-sm">
-                    <p class="text-gray-900 leading-none">Jonathan Reinink</p>
-                    <p class="text-gray-600">Aug 18</p>
+            <section class="flex items-center py-2 hover:bg-[#D3DEDA] hover:rounded-[10px] duration-300 ease-in-out">
+                
+                
+                <img class="w-14 h-14 rounded-full mr-2 bg-black" src="/img/jonathan.jpg" alt="Avatar of Jonathan Reinink" />
+                <div class="text-base font-semibold align-middle items-center justify-center">
+                    <p class="text-gray-900">닉넴1</p>
+                    <p class="text-[#8C8C8C]">소같이 일하자</p>
                 </div>
+            
             </section>
         </>
     )
