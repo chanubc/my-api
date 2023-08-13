8726e3342330df4951a3830e5720553a16b5e170
chanu <cw8881@naver.com>
2023-08-13 20:11:46 +0900

    방문록 영역 스크롤 적용

--- src/components/include/guest/GuestBook.js
+++ src/components/include/guest/GuestBook.js
@@ -6,18 +6,16 @@ const GuestBook = () => {
     return (
         // <!-- 방명록 -->
         <section id="picture"
-            className="picture w-full h-[85%] rounded-[20px] overflow-hidden shadow-lg bg-[#FAFAFA]">
+            className="picture w-full h-[85%] rounded-[20px] overflow-y-auto shadow-lg bg-[#FAFAFA]">
             {/* <!-- 전체 영역 padding 적용 --> */}
             <div className="px-6 py-5">
                 {/* 상단 text 영역 */}
-                <article className='flex items-center mb-3'>
+                <article className='flex h-[50px] items-center mb-3'>
                     <img className='h-[1.1rem] w-[1.1rem] mr-[6px]' src={IconMessage} alt='logo' />
                     <p className="font-bold text-xl">방명록</p>
                 </article>
-
-
-
-
+               
+                
 
             </div>
         </section>
